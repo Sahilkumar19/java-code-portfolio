aca8256cf456fc8891fc8ff96b2ebf3b029a3b57
Sahil Kumar <124178990+Sahilkumar19@users.noreply.github.com>
2023-08-24 12:05:13 +0530

    Linear Search

---
+++ Assignment5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Assignment5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+//        int ans = mySqrt(number);
+//        System.out.println(ans);
+//        int ans = guessNumber(number);
+//        System.out.println(ans);
+    }
+    // 1.Square of a number
+//    static int mySqrt(int num) {
+//        return (int) Math.floor(Math.sqrt(num));
+//    }
+
+    // 2. Guess Number Higher or Lower
+//    static int guessNumber(int n) {
+//    }
+}
