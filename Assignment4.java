411f27d38fbaaba690fe8838c82231ef04316645
Sahil Kumar <124178990+Sahilkumar19@users.noreply.github.com>
2023-08-24 11:29:14 +0530

    Arrays

---
+++ Assignment4.java
@@ -0,0 +1,11 @@
+public class Assignment4 {
+    public static void main(String[] args) {
+
+    }
+    // 1. Build Array from permutation
+
+    
+
+
+
+}
--- Main.java
+++ Main.java
@@ -1,46 +1,37 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        int arr[] = {98,87,76,65,54,43,32,21};
-        int key = 43;
-        int ans = BinarySearch2(arr,key);
-        System.out.println(ans);
-    }
-    // binary search technique for array in ascending order
-    static int BinarySearch(int arr[] , int key) {
-        int s = 0;
-        int e = arr.length-1;
-        while (s <= e) {
-            int mid = s +(e - s)/2;
-            if (arr[mid] > key) {
-                e = mid - 1;
-            } else if (arr[mid] < key) {
-                s = mid + 1;
+        // Q: store a roll number
+        int a = 19;
 
-            }else {
-                return mid;
-            }
+        // Q: store a person's name
+        String name = "Kunal Kushwaha";
+
+        // Q: store 5 roll numbers
+        int rno1 = 23;
+        int rno2 = 55;
+        int rno3 = 18;
+
+        // syntax
+        // datatype[] variable_name = new datatype[size];
+        // store 5 roll numbers:
+//        int[] rnos = new int[5];
+//        // or directly
+//        int[] rnos2 = {23, 12, 45, 32, 15};
+
+        int[] ros; // declaration of array. ros is getting defined in the stack
+        ros = new int[5]; // initialisation: actually here object is being created in the memory (heap)
+
+//        System.out.println(ros[1]);
+        for (int element : ros) {
+            System.out.println(element);
         }
-        return -1;
 
-    }
-    // binary search technique for array in descending order
-    static int BinarySearch2(int arr[] , int key) {
-        int s = 0;
-        int e = arr.length-1;
-        while (s < e) {
-            int mid = s + (e - s)/2;
-            if (arr[mid] > key) {
-                s = mid +1;
-            } else if (arr[mid] < key) {
-                e = mid -1;
-            }else {
-                return mid;
-            }
+        String[] arr = new String[4];
+//        System.out.println(arr[0]);
+
+        for (String element : arr) {
+            System.out.println(element);
         }
-        return -1;
     }
-
-
 }
