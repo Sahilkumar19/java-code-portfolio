8a82b003bbd153cdabe2323cbd13c9d5685d6fd6
Sahil Kumar <kumarsahil7753@gmail.com>
2026-09-04 00:49:20 +0530

    hashing and prefixsum

--- JAVA/newProject/src/Hashing.java
+++ JAVA/newProject/src/Hashing.java
@@ -39,3 +39,97 @@ class TwoSum{
     }
   }
 }
+
+
+
+// 2. subarrays sum equals to k. problem no. 560
+
+// Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+// Example 1:
+
+// Input: nums = [1,1,1], k = 2
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,2,3], k = 3
+// Output: 2
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 2 * 104
+// -1000 <= nums[i] <= 1000
+// -107 <= k <= 107
+
+public int subArraySum(int[] nums, int k){
+ HashMap<Integer, Integer> map = new HashMap<>();
+ map.put(0,1);
+ int currentSum=0;
+ int totalSubArray=0;
+ for(int i=0; i<nums.length; i++){
+  currentSum+=nums[i];
+  if(map.containsKey(currentSum-k){
+   totalSubArray+=map.get(currentSum-k);
+  }
+   map.put(currentSum, map.getOrDefault(currentSum,0)+1);
+ }
+ return totalSubArray;
+}
+
+
+// 3. contiguous array problem no. 525.
+
+// Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+
+ 
+// Example 1:
+
+// Input: nums = [0,1]
+// Output: 2
+// Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
+// Example 2:
+
+// Input: nums = [0,1,0]
+// Output: 2
+// Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+// Example 3:
+
+// Input: nums = [0,1,1,1,1,1,0,0,0]
+// Output: 6
+// Explanation: [1,1,1,0,0,0] is the longest contiguous subarray with equal number of 0 and 1.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// nums[i] is either 0 or 1.
+
+public int findMaxLength(int[] nums){
+ for(int i=0; i<nums.length; i++){
+  if(nums[i]==0){
+   nums[i]=-1;
+  }
+ }
+ HashMap<Integer, Integer> map=new HashMap<>();
+ int maxLength=0;
+ int currentSum=0;
+ for(int i=0; i<nums.length; i++){
+  currentSum+=nums[i];
+  if(currentSum==0){
+   maxLength=i+1;
+  }else if(map.containsKey(currentSum)){
+   maxLength=Math.max(maxLength,i-map.get(currentSum));
+  }else{
+   map.put(currentSum,i);
+  }
+ }
+ return maxLength;
+}
+
+// NOTE: this solution will work(with some tweaking in this sol) in variety of such problem like 
+// ~ max subarray sum equals to zero
+// ~ max subarray sum equal to k
+// and also not just for the binary arrays 
